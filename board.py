5bb74f87935a32be9214f76a53824dc6018ba331
Fraser YT <f_yang-turner23@wincoll.ac.uk>
2026-08-19 20:16:24 +0100

    complete

--- board.py
+++ board.py
@@ -1,3 +1,4 @@
+import copy
 class Board:
     
     def __init__(self): 
@@ -14,9 +15,165 @@ class Board:
         self.white_king_moved  , self.black_king_moved   = False, False
         self.white_a_rook_moved, self.black_a_rook_moved = False, False
         self.white_h_rook_moved, self.black_h_rook_moved = False, False
+        self.white_promotion   , self.black_promotion    = None , None
 
-    def get_legal_moves(x, y):
+        self.white_pieces = {0:  5, 1:  4, 2:  3, 3:  2}
+        self.black_pieces = {7: -5, 6: -4, 5: -3, 4: -2}
+        self.white_en_passant_columns = [False, False, False, False, False, False, False, False]
+        self.black_en_passant_columns = [False, False, False, False, False, False, False, False]
+
+    def get_legal_moves(self, board, y, x):
+
         legal_moves = []
+        piece = board[y][x]
 
+        if piece in [6, -6]:
+            # KING
+            for yx in [[1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1]]:
+                if y + yx[0] >= 0 and y + yx[0] <= 7 and x + yx[1] >= 0 and x + yx[1] <= 7:
+                    if board[y + yx[0]][x + yx[1]] * piece <= 0:
+                        legal_moves.append([y + yx[0], x + yx[1]])
+
+            if not self.white_king_moved:
+                if not self.white_h_rook_moved:
+                    if board[7][5] == 0 and board[7][6] == 0 and board[7][7] == 4:
+                        legal_moves.append([7, 6])
+                if not self.white_a_rook_moved:
+                    if board[7][1] == 0 and board[7][2] == 0 and board[7][3] == 0 and board[7][0] == 4:
+                        legal_moves.append([7, 2])
+
+            if not self.black_king_moved:
+                if not self.black_h_rook_moved:
+                    if board[0][5] == 0 and board[0][6] == 0 and board[0][7] == -4:
+                        legal_moves.append([0, 6])
+                if not self.black_a_rook_moved:
+                    if board[0][1] == 0 and board[0][2] == 0 and board[0][3] == 0 and board[0][0] == -4:
+                        legal_moves.append([0, 2])
+
+        if piece in [5, -5, 4, -4, 3, -3]:
+
+            if piece in [5, -5]:
+                # QUEEN
+                directions = [[1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1]]
+            if piece in [4, -4]:
+                # ROOK
+                directions = [[1, 0], [0, -1], [-1, 0], [0, 1]]
+            if piece in [3, -3]:
+                # BISHOP
+                directions = [[1, 1], [1, -1],  [-1, -1], [-1, 1]]
+
+            for yx in directions:
+                while True:
+                    if y + yx[0] >= 0 and y + yx[0] <= 7 and x + yx[1] >= 0 and x + yx[1] <= 7:
+                        if board[y + yx[0]][x + yx[1]] * piece < 0:
+                            legal_moves.append([y + yx[0], x + yx[1]])
+                            break
+                        elif board[y + yx[0]][x + yx[1]] == 0:
+                            legal_moves.append([y + yx[0], x + yx[1]])
+                        else:
+                            break
+                    else:
+                        break
+
+                    yx[0] = yx[0] + 1 if yx[0] >= 1 else yx[0]
+                    yx[0] = yx[0] - 1 if yx[0] <= -1 else yx[0]
+                    yx[1] = yx[1] + 1 if yx[1] >= 1 else yx[1]
+                    yx[1] = yx[1] - 1 if yx[1] <= -1 else yx[1]
+            
+        if piece in [2, -2]:
+            # KNIGHT
+            for yx in [[2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2], [1, 2]]:
+                if y + yx[0] >= 0 and y + yx[0] <= 7 and x + yx[1] >= 0 and x + yx[1] <= 7:
+                    if board[y + yx[0]][x + yx[1]] * piece <= 0:
+                        legal_moves.append([y + yx[0], x + yx[1]])
+
+        if piece in [1, -1]:
+            # PAWN
+            if board[y - piece][x] == 0:
+                legal_moves.append([y - piece, x])
+                if (y == 1 and board[y + 2][x] == 0 and piece == -1) or (y == 6 and board[y - 2][x] == 0 and piece == 1):
+                    legal_moves.append([y - 2 * piece, x])
+
+            for yx in [[-1 * piece, -1], [-1 * piece, 1]]:
+                if x + yx[1] >= 0 and x + yx[1] <= 7:
+                    if board[y + yx[0]][x + yx[1]] * piece < 0:
+                        legal_moves.append([y + yx[0], x + yx[1]])
+                        
+                    if piece == 1 and y == 3 and board[y + yx[0]][x + yx[1]] == 0 and self.black_en_passant_columns[x + yx[1]] == True:
+                        legal_moves.append([y + yx[0], x + yx[1]])
+                    if piece == -1 and y == 4 and board[y + yx[0]][x + yx[1]] == 0 and self.white_en_passant_columns[x + yx[1]] == True:
+                        legal_moves.append([y + yx[0], x + yx[1]])
+            
         return legal_moves
 
+    def refine_legal_moves(self, boardcopy, y, x, legal_moves, current_colour):
+        refined_moves = []
+
+        for move in legal_moves:
+            board = copy.deepcopy(boardcopy)
+
+            castle_out_of_check = False
+
+            if not self.white_king_moved:
+                if board[y][x] == 6:
+                    if move[1] == 6:
+                        board[y][5] = 6
+                        castle_out_of_check = True
+                    elif move[1] == 2:
+                        board[y][3] = 6
+                        castle_out_of_check = True
+
+            if not self.black_king_moved:
+                if board[y][x] == -6:
+                    if move[1] == 6:
+                        board[y][5] = -6
+                        castle_out_of_check = True
+                    elif move[1] == 2:
+                        board[y][3] = -6
+                        castle_out_of_check = True 
+
+            if abs(board[y][x]) == 1 and board[move[0]][move[1]] == 0 and x != move[1]:
+                board[y][move[1]] = 0
+
+            if (board[y][x] == 1 and move[0] == 0) or (board[y][x] == -1 and move[0] == 7):
+                board[y][x] = board[y][x] * 5
+
+            board[move[0]][move[1]] = board[y][x]
+            board[y][x] = 0 if not castle_out_of_check else board[y][x]
+
+            white_controlled, black_controlled = self.checks(board)
+            king_in_check = False
+
+            for row in range(8):
+                for col in range(8):
+                    if board[row][col] == 6 and [row, col] in black_controlled and current_colour == "White":
+                        king_in_check = True
+                    if board[row][col] == -6 and [row, col] in white_controlled and current_colour == "Black": 
+                        king_in_check = True
+
+            if not king_in_check:
+                refined_moves.append(move)
+                        
+        return refined_moves
+
+    def checks(self, board):
+
+        white_controlled, black_controlled = [], []
+
+        for row in range(8):
+            for col in range(8):
+
+                if board[row][col] > 0:
+                    controlled = self.get_legal_moves(board, row, col)
+                    for square in controlled:
+                        if square not in white_controlled:
+                            white_controlled.append(square)
+
+                elif board[row][col] < 0:
+                    controlled = self.get_legal_moves(board, row, col)
+                    for square in controlled:
+                        if square not in black_controlled:
+                            black_controlled.append(square)
+
+        return white_controlled, black_controlled
+                                            
--- game.py
+++ game.py
@@ -8,14 +8,18 @@ class Game:
         pygame.init()
         self.board = Board()
 
-        self.screen_size = 1000
-        self.square_size = 100
-        self.piece_size = 80
-        self.board_size = 8
-
         self.start_square = None
         self.end_square = None
         self.current = "White"
+        self.legal_moves = []
+
+        self.white_controlled = []
+        self.black_controlled = []
+        self.white_king_checked = None
+        self.black_king_checked = None
+
+        self.total_white_moves = []
+        self.total_black_moves = []
 
         self.white_win = False
         self.black_win = False
@@ -23,23 +27,23 @@ class Game:
 
         self.piece_images = {
 
-            1: pygame.transform.smoothscale(pygame.image.load('w_pawn_1024px.png'), (self.piece_size, self.piece_size)),
-            2: pygame.transform.smoothscale(pygame.image.load('w_knight_1024px.png'), (self.piece_size, self.piece_size)),
-            3: pygame.transform.smoothscale(pygame.image.load('w_bishop_1024px.png'), (self.piece_size, self.piece_size)),
-            4: pygame.transform.smoothscale(pygame.image.load('w_rook_1024px.png'), (self.piece_size, self.piece_size)),
-            5: pygame.transform.smoothscale(pygame.image.load('w_queen_1024px.png'), (self.piece_size, self.piece_size)),
-            6: pygame.transform.smoothscale(pygame.image.load('w_king_1024px.png'), (self.piece_size, self.piece_size)),
+            1: pygame.transform.smoothscale(pygame.image.load('w_pawn_1024px.png'), (75, 75)),
+            2: pygame.transform.smoothscale(pygame.image.load('w_knight_1024px.png'), (75, 75)),
+            3: pygame.transform.smoothscale(pygame.image.load('w_bishop_1024px.png'), (75, 75)),
+            4: pygame.transform.smoothscale(pygame.image.load('w_rook_1024px.png'), (75, 75)),
+            5: pygame.transform.smoothscale(pygame.image.load('w_queen_1024px.png'), (75, 75)),
+            6: pygame.transform.smoothscale(pygame.image.load('w_king_1024px.png'), (75, 75)),
 
-            -1: pygame.transform.smoothscale(pygame.image.load('b_pawn_1024px.png'), (self.piece_size, self.piece_size)),
-            -2: pygame.transform.smoothscale(pygame.image.load('b_knight_1024px.png'), (self.piece_size, self.piece_size)),
-            -3: pygame.transform.smoothscale(pygame.image.load('b_bishop_1024px.png'), (self.piece_size, self.piece_size)),
-            -4: pygame.transform.smoothscale(pygame.image.load('b_rook_1024px.png'), (self.piece_size, self.piece_size)),
-            -5: pygame.transform.smoothscale(pygame.image.load('b_queen_1024px.png'), (self.piece_size, self.piece_size)),
-            -6: pygame.transform.smoothscale(pygame.image.load('b_king_1024px.png'), (self.piece_size, self.piece_size)),
+            -1: pygame.transform.smoothscale(pygame.image.load('b_pawn_1024px.png'), (75, 75)),
+            -2: pygame.transform.smoothscale(pygame.image.load('b_knight_1024px.png'), (75, 75)),
+            -3: pygame.transform.smoothscale(pygame.image.load('b_bishop_1024px.png'), (75, 75)),
+            -4: pygame.transform.smoothscale(pygame.image.load('b_rook_1024px.png'), (75, 75)),
+            -5: pygame.transform.smoothscale(pygame.image.load('b_queen_1024px.png'), (75, 75)),
+            -6: pygame.transform.smoothscale(pygame.image.load('b_king_1024px.png'), (75, 75))
 
         }
 
-        self.screen = pygame.display.set_mode((self.screen_size, self.screen_size))
+        self.screen = pygame.display.set_mode((1000, 1000))
         self.font = pygame.font.SysFont(None, 36)
         pygame.display.set_caption("Chess Program")
         self.clock = pygame.time.Clock()
@@ -51,65 +55,224 @@ class Game:
             if event.type == pygame.QUIT:
                 self.running = False
 
+            self.white_controlled, self.black_controlled = self.board.checks(self.board.state)
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:       
                     mouse_x, mouse_y = pygame.mouse.get_pos()
                     if mouse_x >= 100 and mouse_x <= 900 and mouse_y >= 100 and mouse_y <= 900:
                         square_x, square_y = mouse_x // 100 - 1, mouse_y // 100 - 1
                         
-                        # if self.start_square is None and ((self.board.state[square_y][square_x] > 0 and self.current == "White") or (self.board.state[square_y][square_x] < 0 and self.current == "Black")):
-                        #     self.start_square = [square_x, square_y]
-                        # elif self.start_square is not None and (square_x != self.start_square[1] or square_y != self.start_square[0]):
-                        #     self.end_square = [square_x, square_y]
+                        if self.start_square is None and ((self.board.state[square_y][square_x] > 0 and self.current == "White") or (self.board.state[square_y][square_x] < 0 and self.current == "Black")):
+                            self.start_square = [square_y, square_x]
+                            self.legal_moves = self.board.get_legal_moves(self.board.state, self.start_square[0], self.start_square[1])
+                            self.legal_moves = self.board.refine_legal_moves(self.board.state, self.start_square[0], self.start_square[1], self.legal_moves, self.current)
 
-                        #     # normal piece movement
-                        #     if (self.board.state[self.start_square[1]][self.start_square[0]] * self.board.state[self.end_square[1]][self.end_square[0]]) <= 0:
+                        elif self.start_square is not None and (square_y != self.start_square[0] or square_x != self.start_square[1]):
+                            self.end_square = [square_y, square_x]
+                    
+                            if [self.end_square[0], self.end_square[1]] in self.legal_moves:
 
-                        #         self.board.state[self.end_square[1]][self.end_square[0]] = self.board.state[self.start_square[1]][self.start_square[0]]
-                        #         self.board.state[self.start_square[1]][self.start_square[0]] = 0
+                                if not self.board.white_king_moved:
+                                    if self.board.state[self.start_square[0]][self.start_square[1]] == 6:
+                                        self.board.white_king_moved = True
+
+                                        if self.end_square[1] == 6:
+                                            self.board.white_h_rook_moved = True
+                                            self.board.state[7][5] = 4
+                                            self.board.state[7][7] = 0
+
+                                        elif self.end_square[1] == 2:
+                                            self.board.white_a_rook_moved = True
+                                            self.board.state[7][3] = 4
+                                            self.board.state[7][0] = 0
+
+                                if not self.board.black_king_moved:
+                                    if self.board.state[self.start_square[0]][self.start_square[1]] == -6:
+                                        self.board.black_king_moved = True 
+
+                                        if self.end_square[1] == 6:
+                                            self.board.black_h_rook_moved = True
+                                            self.board.state[0][5] = -4
+                                            self.board.state[0][7] = 0
+
+                                        elif self.end_square[1] == 2:
+                                            self.board.black_a_rook_moved = True
+                                            self.board.state[0][3] = -4
+                                            self.board.state[0][0] = 0
+
+                                if self.board.state[self.start_square[0]][self.start_square[1]] == 4:
+                                    if self.start_square[0] == 7 and self.start_square[1] == 7:
+                                        self.board.white_h_rook_moved = True
+                                    elif self.start_square[0] == 7 and self.start_square[1] == 0:
+                                        self.board.white_a_rook_moved = True
+                                elif self.board.state[self.start_square[0]][self.start_square[1]] == -4:
+                                    if self.start_square[0] == 0 and self.start_square[1] == 7:
+                                        self.board.black_h_rook_moved = True
+                                    elif self.start_square[0] == 0 and self.start_square[1] == 0:
+                                        self.board.black_a_rook_moved = True
+
+                                if abs(self.board.state[self.start_square[0]][self.start_square[1]]) == 1 and self.board.state[self.end_square[0]][self.end_square[1]] == 0 and self.start_square[1] != self.end_square[1]:
+                                    self.board.state[self.start_square[0]][self.end_square[1]] = 0
+
+                                self.board.white_en_passant_columns = [False, False, False, False, False, False, False, False] 
+                                if self.board.state[self.start_square[0]][self.start_square[1]] == 1 and self.start_square[0] - self.end_square[0] == 2:
+                                    self.board.white_en_passant_columns[self.start_square[1]] = True
+
+                                self.board.black_en_passant_columns = [False, False, False, False, False, False, False, False] 
+                                if self.board.state[self.start_square[0]][self.start_square[1]] == -1 and self.start_square[0] - self.end_square[0] == -2:
+                                    self.board.black_en_passant_columns[self.start_square[1]] = True
+
+                                if self.board.state[self.start_square[0]][self.start_square[1]] == 1 and self.end_square[0] == 0:
+                                    self.board.white_promotion = self.end_square[1]
+                                if self.board.state[self.start_square[0]][self.start_square[1]] == -1 and self.end_square[0] == 7:
+                                    self.board.black_promotion = self.end_square[1]
+
+                                self.board.state[self.end_square[0]][self.end_square[1]] = self.board.state[self.start_square[0]][self.start_square[1]]
+                                self.board.state[self.start_square[0]][self.start_square[1]] = 0
+
+                                if self.board.white_promotion in [0, 1, 2, 3, 4, 5, 6, 7] or self.board.black_promotion in [0, 1, 2, 3, 4, 5, 6, 7]:
+
+                                    if self.board.white_promotion in [0, 1, 2, 3, 4, 5, 6, 7]:
+                                        for p in [0, 1, 2, 3]:
+                                            pygame.draw.circle(self.screen, "#BBBBBB", (self.board.white_promotion * 100 + 150, p * 100 + 150), 50)
+                                            self.screen.blit(self.piece_images[self.board.white_pieces[p]], (self.board.white_promotion * 100 + 112.5, p * 100 + 112.5))
+
+                                    if self.board.black_promotion in [0, 1, 2, 3, 4, 5, 6, 7]:
+                                        for p in [7, 6, 5, 4]:
+                                            pygame.draw.circle(self.screen, "#BBBBBB", (self.board.black_promotion * 100 + 150, p * 100 + 150), 50)
+                                            self.screen.blit(self.piece_images[self.board.black_pieces[p]], (self.board.black_promotion * 100 + 112.5, p * 100 + 112.5))
+
+                                    pygame.display.flip()  
+                                    self.clock.tick(60)
+
+                                    complete = False
+                                    while True:
+
+                                        for event in pygame.event.get():
+                                            if event.type == pygame.QUIT:
+                                                self.running = False
                                 
-                        #         self.start_square = None
-                        #         self.current = "White" if self.current == "Black" else "Black"
-                                
-                        #     elif self.board.state[self.end_square[1]][self.end_square[0]] != 0:
-                        #         self.start_square = [self.end_square[0], self.end_square[1]]
+                                            if event.type == pygame.MOUSEBUTTONDOWN:
+                                                if event.button == 1:       
+                                                    mouse_x, mouse_y = pygame.mouse.get_pos()
+                                                    square_x, square_y = mouse_x // 100 - 1, mouse_y // 100 - 1
+
+                                                    if self.board.white_promotion == square_x:
+                                                        if square_y in [0, 1, 2, 3]:
+                                                            self.board.state[self.end_square[0]][self.end_square[1]] = self.board.white_pieces[square_y]
+                                                            self.board.white_promotion = None
+                                                            complete = True
+
+                                                    if self.board.black_promotion == square_x:
+                                                        if square_y in [7, 6, 5, 4]:
+                                                            self.board.state[self.end_square[0]][self.end_square[1]] = self.board.black_pieces[square_y]
+                                                            self.board.black_promotion = None
+                                                            complete = True
+                                            
+                                        if complete:
+                                            break
+
+                                self.start_square = None
+                                self.legal_moves = []
+                                self.current = "White" if self.current == "Black" else "Black"
+                            
+                            elif self.board.state[self.end_square[0]][self.end_square[1]] * self.board.state[self.start_square[0]][self.start_square[1]] > 0:
+                                self.start_square = [self.end_square[0], self.end_square[1]]
+                                self.legal_moves = self.board.get_legal_moves(self.board.state, self.start_square[0], self.start_square[1])
+                                self.legal_moves = self.board.refine_legal_moves(self.board.state, self.start_square[0], self.start_square[1], self.legal_moves, self.current)
+
+                            else:
+                                self.start_square = None
+                                self.legal_moves = []
 
                     else:
                         self.start_square = None
+                        self.legal_moves = []
 
+    def check_result(self):
 
+        self.white_king_checked = None
+        self.black_king_checked = None
+        self.total_white_moves = []
+        self.total_black_moves = []
+
+        for row in range(8):
+            for col in range(8):
+
+                if self.board.state[row][col] == 6 and [row, col] in self.black_controlled:
+                    self.white_king_checked = [row, col]
+                if self.board.state[row][col] == -6 and [row, col] in self.white_controlled:
+                    self.black_king_checked = [row, col]
+
+                l_moves = self.board.get_legal_moves(self.board.state, row, col)
+                l_moves = self.board.refine_legal_moves(self.board.state, row, col, l_moves, self.current)
+                
+                if l_moves != [] and self.board.state[row][col] > 0:
+                    self.total_white_moves.append(l_moves)
+                if l_moves != [] and self.board.state[row][col] < 0:
+                    self.total_black_moves.append(l_moves)
+
+        if self.current == "White" and self.total_white_moves == []:
+            if self.white_king_checked is not None:
+                self.black_win = True
+            else:
+                self.draw = True
+        if self.current == "Black" and self.total_black_moves == []:
+            if self.black_king_checked is not None:
+                self.white_win = True
+            else:
+                self.draw = True
 
     def draw_board(self):
 
         self.screen.fill("#000000")
-        team = self.font.render(f"{self.current} to play", True, (255, 255, 255))
-        self.screen.blit(team, (420, 50))
+        self.team = self.font.render(f"{self.current} to play", True, (255, 255, 255))
+        self.screen.blit(self.team, (420, 50))
 
-        for row in range(self.board_size):
-            for col in range(self.board_size):
+        for row in range(8):
+            for col in range(8):
 
-                x = (col + 1) * self.square_size 
-                y = (row + 1) * self.square_size
-                colour = "#4E3005" if (row + col) % 2 else "#8A775C"
+                x = (col + 1) * 100 
+                y = (row + 1) * 100
+
+                colour = "#4E3005" if (row + col) % 2 else "#8A775C"          
+                pygame.draw.rect(self.screen, colour, (x, y, 100, 100))
+
                 piece = self.board.state[row][col]
-
-                pygame.draw.rect(self.screen, colour, (x, y, self.square_size, self.square_size))
                 if piece != 0:
-                    self.screen.blit(self.piece_images[piece], (x + 0.1 * self.square_size, y + 0.1 * self.square_size))
+                    self.screen.blit(self.piece_images[piece], (x + 12.5, y + 12.5))
 
         if self.start_square is not None:
-            pygame.draw.rect(self.screen, "#0A2E13", (self.start_square[0] * 100 + 100, self.start_square[1] * 100 + 100, self.square_size, self.square_size), 4)
+            pygame.draw.rect(self.screen, "#0A2E13", (self.start_square[1] * 100 + 100, self.start_square[0] * 100 + 100, 100, 100), 4)
+        
+            for position in self.legal_moves:
+                pygame.draw.circle(self.screen, "#272727", (position[1] * 100 + 150, position[0] * 100 + 150), 15)
 
-        legal_moves = self.board.get_legal_moves()
-        for position in legal_moves:
-            pygame.draw.circle(self.screen, "#272727", (position[1] * 100 + 150, position[0] * 100 + 150), 15, 3)
-
+        if self.white_king_checked is not None:
+            pygame.draw.rect(self.screen, "#D94610", (self.white_king_checked[1] * 100 + 105, self.white_king_checked[0] * 100 + 105, 90, 90), 4)
+        if self.black_king_checked is not None:
+            pygame.draw.rect(self.screen, "#D94610", (self.black_king_checked[1] * 100 + 105, self.black_king_checked[0] * 100 + 105, 90, 90), 4)
+ 
     def run(self):
         while self.running:
+
+            if self.white_win:
+                print("White won!")
+                break
+            if self.black_win:
+                print("Black won!")
+                break
+            if self.draw:
+                print("It was a draw!")
+                break
+
             self.check_click()
+            self.check_result()
             self.draw_board()
             pygame.display.flip()
             self.clock.tick(60)
+
         pygame.quit()
 
 game = Game()
